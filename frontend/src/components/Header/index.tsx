2253dc37e4486a23ab01c89aff8def8053ce3336
Rafael Maia <rafaelmaia3002@gmail.com>
2022-11-17 13:07:46 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="Logo DSMeta" />
+                <h1>DSMeta</h1>
+                <p>Replicado por <a href="https://github.com/rafaelmaiia" target="_blank" rel="external">Rafael Maia</a> / Projeto Semana Spring React -
+                <a href="https://www.instagram.com/devsuperior.ig" target="_blank" rel="external">Nelio Alves</a></p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
