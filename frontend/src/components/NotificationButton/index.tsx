494c056d65b9ca16376f673bb32bfa2bd65f14ed
Rafael Maia <rafaelmaia3002@gmail.com>
2022-11-17 13:28:25 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
-import icon from '../../assets/img/notification-icon.svg'
+import icon from '../../assets/img/notification-icon.svg';
 
-import './styles.css'
+import './styles.css';
 
 function NotificationButton() {
     return (
@@ -9,4 +9,4 @@ function NotificationButton() {
         </div>
     )
 }
-export default NotificationButton
+export default NotificationButton;
