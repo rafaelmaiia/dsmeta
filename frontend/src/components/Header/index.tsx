d9e319bc44820deaa7e94efd1c84dc3b092d5f67
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2023-05-25 19:21:15 -0300

    Update index.tsx

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -8,8 +8,8 @@ function Header() {
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="Logo DSMeta" />
                 <h1>DSMeta</h1>
-                <p>Replicado por <a href="https://github.com/rafaelmaiia" target="_blank" rel="external">Rafael Maia</a> / Projeto Semana Spring React -
-                <a href="https://www.instagram.com/devsuperior.ig" target="_blank" rel="external">Nelio Alves</a></p>
+                <p>Desenvolvido por <a href="https://github.com/rafaelmaiia" target="_blank" rel="external">Rafael Maia</a> / Projeto Semana Spring React -
+                <a href="https://www.instagram.com/devsuperior.ig" target="_blank" rel="external"> Nelio Alves</a></p>
             </div>
         </header>
     )
